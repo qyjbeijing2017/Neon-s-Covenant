915348bad3ab73191e0b578783eb216fbab5b782
LAPTOP-U8N94B6A\qyjbe <1798174328@qq.com>
2018-04-09 21:24:58 +0800

    增加了小怪的动画、动画机。

---
+++ Assets/Scripts/AttackNear.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackNear : MonoBehaviour
+{
+
+	public Animator anim;
+	public int attackColor;
+	public float attackValue;
+	public bool attackOther;
+	public GameObject otherObject;
+
+	public void SetProperty(int color, float value)
+	{
+		attackColor = color;
+		attackValue = value;
+
+		attackOther = false;
+	}
+
+	void OnTriggerEnter(Collider collider)
+	{
+		//if (collider.name != "Ground" && collider.tag != "woodenStick")
+		//{
+		//	collider.GetComponent<CBehaviour>().GetHit(attackColor, attackValue, 0);
+		//}
+		Debug.Log(attackColor);
+		Debug.Log(attackValue);
+		if (collider.tag =="enemy")
+		{
+			collider.GetComponent<CBehaviour>().GetHit(attackColor, attackValue, 0);
+		}
+
+
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+
+	}
+}
