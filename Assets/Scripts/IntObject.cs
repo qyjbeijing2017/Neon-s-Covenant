120cea6fd82257eda34426adc4609aacb13d93d7
shpkng <shpkng@gmail.com>
2018-04-20 13:57:04 +0800

    4-20
    fuck Liu Qiangdong

---
+++ Assets/Scripts/IntObject.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntObject : MonoBehaviour
+{
+
+	[SerializeField] GameObject notification;   //用来提示
+	[SerializeField] UnityEngine.UI.Text text;
+	[SerializeField] string content;
+	enum TypeOfGizmo { 告示牌, 机关, 其他 };
+	[SerializeField] TypeOfGizmo 物体类型;
+	bool trigger;
+
+	bool mCharIn = false;   //主角是否在控制范围中
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "MainCharacter")
+		{
+			mCharIn = true;
+			Display();
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "MainCharacter")
+		{
+			mCharIn = false;
+			Display();
+		}
+	}
+
+	void Display() //显示\隐藏提示板
+	{
+		if (mCharIn)
+		{
+			text.text = content;
+		}
+		else
+		{
+			text.text = "";
+		}
+	}
+
+	void Start()
+	{
+
+	}
+
+	private void Update()
+	{
+		if (mCharIn && Input.GetKeyDown(KeyCode.E))
+		{
+			switch (物体类型)
+			{
+				case TypeOfGizmo.告示牌: { CanvasEffect.Diffuse(); CanvasEffect.DisplayNotice(); } break;
+				case TypeOfGizmo.机关:; break;
+				case TypeOfGizmo.其他:; break;
+			}
+		}
+	}
+}
